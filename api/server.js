fa82909d6082b28878d329c4d50c5823530fbcd6
edeliscoding <edelreichd@gmail.com>
2024-03-20 12:01:42 -0600

    added scripts and served frontend files in the backend express

--- api/server.js
+++ api/server.js
@@ -4,13 +4,22 @@ const dbConnect = require("./dbConnect");
 const movieRoutes = require("./routes/movies");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
+// const __dirname = path.resolve();
 dbConnect();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", movieRoutes);
+
+// console.log(__dirname);
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
